e68b402d777be78a0d53c168272ca1ff9d242670
RustamBH <kamaji@mail.ru>
2022-02-24 00:20:38 +0400

    Added file task6_1.py

---
+++ task6_1.py
@@ -0,0 +1,23 @@
+﻿cook_book = {}
+
+with open('tmp/recipes_win1251.txt', 'rt') as fp:
+    while True:
+        dish = fp.readline().strip()
+        if len(dish) > 0:
+            ingred_num = int(fp.readline().strip())
+            recipe_list = []
+            for i in range(0, ingred_num):
+                recipe_dict = {}
+                recipe_str = fp.readline().strip()
+                ingred_line = recipe_str.split(' | ')
+                recipe_dict['ingredient_name'] = ingred_line[0]
+                recipe_dict['quantity'] = int(ingred_line[1])
+                recipe_dict['measure'] = ingred_line[2]
+                recipe_list.append(recipe_dict)
+
+            cook_book[dish] = recipe_list
+            space = fp.readline().strip()
+        else:
+            break
+
+print(cook_book)
